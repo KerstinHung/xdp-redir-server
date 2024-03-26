c123b6062958da3a8b1572eb36068f0ad2694e04
Kerstin Hung <tenwu06@gmail.com>
2024-03-26 15:47:38 +0800

    user code get map

--- xdp_redir_user.c
+++ xdp_redir_user.c
@@ -16,6 +16,9 @@ struct xdp_program *prog = NULL;
 
 int main(int argc, char *argv[])
 {
+    int prog_fd, map_fd, ret;
+    struct bpf_object *bpf_obj;
+
     /* get the interface index by name */
     ifindex = if_nametoindex(argv[1]);
     if (!ifindex) {
@@ -25,4 +28,23 @@ int main(int argc, char *argv[])
 
     /* load XDP object by libxdp */
     prog = xdp_program__open_file("xdp_redir_kern.o", "xdp_redir", NULL);
+    if (!prog) {
+        priintf("Error, load xdp prog failed\n");
+        return ;
+    }
+
+    /* attach the XDP program to the net device XDP hook */
+    ret = xdp_program__attach(prog, ifindex, XDP_MODE_SKB, 0);
+    if (ret) {
+        printf("Error, Set xdp fd on %d failed\n", ifindex);
+        return ret;
+    }
+
+    /* Find the map fd from bpf object */
+    bpf_obj = xdp_program__bpf_obj(prog);
+    map_fd = bpf_object__find_map_by_name(bpf_obj, "servers");
+    if (map_fd < 0) {
+        printf("Error, get map fd from bpf obj failed\n");
+        return map_fd;
+    }
 }
